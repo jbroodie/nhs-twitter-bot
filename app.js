9e3161d55c72f85cf4f3fa1ef48dd66e90158ef3
Johnathon Broodie <johnathon.broodie@nhs.net>
2016-12-15 10:08:01 +0000

    :triumph: Setup to deploy in Heroku

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 var Twitter = require('twitter');
-var env = require('dotenv').config();
+var env = process.env.consumer_key ? process.env : require('dotenv').config();
 var async = require('async');
 var request = require('request');
 var postcodesIO = require('postcodesio-client');
-var log4js = require( "log4js" );
+var log4js = require('log4js');
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
+// For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+var secret = {
+    consumer_key: env.consumer_key, 
+    consumer_secret: env.consumer_secret, 
+    access_token_key: env.access_token, 
+    access_token_secret: env.access_token_secret   
+};
+var client = new Twitter(secret);
+
+log4js.loadAppender('file');
+log4js.configure({
+    appenders: [
+        {
+            type:'file',
+            filename: 'twitbot.log',
+            category: 'debug',
+            maxLogSize: 20480,
+            backups: 10
+        }
+    ]
+})
+var logger = log4js.getLogger('debug');
+log("Started running...");
+
+// set things going
+var trackAndFollowObj= {
+    //track: 'help,nearest pharmacy,what is'
+    //follow: 'AndyDevTest'
+    //'follow=AndyDevTest'
+    //{track: 'help,nearest pharmacy,what is'},
+    //{follow: '799761379150495744'}
+    //track: 'help,nearest pharmacy,what is',
+    //follow: '799761379150495744'
+    follow: env.this_account_id
+}; 
+
+//var result = getTweetPostcode("nearest pharmacy S42 6RP and something else")
+client.stream('statuses/filter', trackAndFollowObj, streamTweet);
+
+// Private functions
 // log some output - this needs changing to Bunyan!
 function log(logString){
     console.log(logString);
@@ -13,108 +63,26 @@ function log(logString){
 
 }
 
-// Does the source tweeter follow us?
-function checkIfFollower(data, reqField) {
-    
-    var connectionField = data[0].connections[0];
+function streamTweet (stream) {
+    // callBack for when we get tweet data...
+    stream.on('data', onTweet);
 
-    if (connectionField == "followed_by"){   // connections = 'followed by'' for a follower
-        log("checkIfFollower connection field value = " + connectionField + ". Returning TRUE");
-        return true;
-    }
-
-    return false;
-}
-
-// parse the tweet string for a postcode
-function getTweetPostcode(tweetString){
-    var postcodeRegEx = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z]0-9][A-Za-z])|([A-Z-z][A-Ha-hJ-Yj-y][09]?[A-Za-z])))) {0,1}[0-9][A-Za-z]{2})";
-    var match = tweetString.match(postcodeRegEx);
-
-    var tweetPostcode = null;
-    if(match != null){
-        tweetPostcode = match[0];// just assume the first postcode in the array is the one we need
-    }
-    log("getTweetPostcode: Match[0] is " + tweetPostcode);
-
-    return tweetPostcode;
-}
-
-// call to the API to convert postcode to lat/lon
-function getLongLatFromPostcode(postCode, callback)
-{
-    var longLatValue = {};
-    var postcodesAPI = new postcodesIO();
-    postcodesAPI.lookup(postCode, function (error, longLatValue) {
-        if(error){
-            log("getLongLatFromPostcode - postcodesAPI error = " + error);
-            return callback(error);
-        }
-
-        callback(null, longLatValue);
+    // ... when we get an error...
+    stream.on('error', function(error) {
+        log("function streamTweet error:" + error);
     });
 }
 
-// using long and lat, look up the nearest pharmacy
-function getNearestPharmFromLongLat(geoLocation, callback){
-
-    var serviceReply = {};
-
-    var requestUrl = env.nearby_service_api + "latitude=" + geoLocation.latitude + "&" + "longitude=" + geoLocation.longitude;
-    request(requestUrl, function (error, response, body) {
-        if(error){
-            log(error);
-            return callback(error);
-        }
-
-        if (response.statusCode != 200) {
-            return callback(new Error("getNearestPharmFromLogLat = url did not return 200"));
-        }
-        
-        // convert the nearest response into a reply
-       serviceReply.serviceLocResolved = true;
-
-        var responseStuff = JSON.parse(body);
-        var firstNearbyPharm = responseStuff.nearby[0];
-        var nearbyName = firstNearbyPharm.name;
-        var nearbyMessage = firstNearbyPharm.openingTimesMessage;
-        var address = firstNearbyPharm.address;
-        // if fields are null, then don't add them to the location info
-        var add1 = (address.line1=="" ? "": (address.line1 + ", "));
-        var add2 = (address.line2=="" ? "": (address.line2 + ", "));
-        var add3 = (address.line3=="" ? "": (address.line3 + ", "));
-        var add4 = (address.city=="" ? "": (address.city + ", "));
-        var postcode = (address.postcode=="" ? "": address.postcode);
-
-        serviceReply.text = "Your nearest Pharmacy is " + nearbyName + ". "
-            + add1 + add2 + add3 + add4 + postcode + ". " + nearbyMessage;
-        log("Data from nearest to return is " + serviceReply.text);
+function onTweet(tweet) {
+    // print out the text of the tweet that came in
+    log(
+        "onTweet: Just received this tweet: " + tweet.text + 
+        " from " + tweet.user.screen_name);
     
-        callback(null, serviceReply);
-    });
-}
+    var tweetTextLC = tweet.text.toLowerCase();
 
-function createStatusUpdateObj(screen_name, currentTweetText, tweetInfo)
-{
-    var StatusObj = {};
-    
-    if(tweetInfo.isFollower == true){
-        if(tweetInfo.serviceLocResolved){
-            statusObj = {status: "D " + screen_name + " " + currentTweetText };
-        }else{
-            if(tweetInfo.badPostcode == true){
-                statusObj = {status: "D " + screen_name + " Thanks for the tweet about nearest Pharmacy. Sorry, could not find a postcode that matches the one you sent. Please check and try again"};
-            }else{
-                statusObj = {status: "D " + screen_name + " Thanks for the tweet about nearest Pharmacy. I need a bit more info on your location. Tweet me your postcode or turn on tweet location in your profile. Thanks "};
-            }
-        }
-    }
-    else{
-        statusObj = {status: "@" + screen_name + ". I'm a bot. For best results, follow me & tweet me in form 'Nearest Pharmacy <your postcode> and I'll DM a reply"};
-    }
-    statusObj.status;
-
-    return statusObj;
+    // for now, just focus on nearest pharmacy
+    respondToNearestPharm(client, tweet);
 }
 
 // respondToNearestPharm query
@@ -225,98 +193,106 @@ function respondToNearestPharm(client, tweet) {
         });
 }
 
-function replyWithHelp(client, screenName) {
-
-}
-
-function respondToCondition(client, screenName) {
-    // TODO
-}
-
-
-function onTweet(tweet) {
-    // print out the text of the tweet that came in
-    log("onTweet: Just received this tweet: " + tweet.text + " from " + tweet.user.screen_name);
+// Does the source tweeter follow us?
+function checkIfFollower(data, reqField) {
     
-    var tweetTextLC = tweet.text.toLowerCase();
+    var connectionField = data[0].connections[0];
 
-    // for now, just focus on nearest pharmacy
-    respondToNearestPharm(client, tweet);
-/*
-    // work out what has been tweeted
-    if(tweetTextLC.indexOf("I'm a bot. For best results follow me & tweet me in the form:") > -1){
-        //ignore - this is us consuming our own tweet
+    if (connectionField == "followed_by"){   // connections = 'followed by'' for a follower
+        log("checkIfFollower connection field value = " + connectionField + ". Returning TRUE");
+        return true;
     }
-    else if(tweetTextLC.indexOf("help") > -1){
-        // tweet is a help one
-        //replyWithHelp(client, tweet.user.screen_name);
-    }
-    else if (tweetTextLC.indexOf("nearest pharmacy") > -1){
-        // tweet seeking nearest pharmacy
-        respondToNearestPharm(client, tweet);
-    }
-    else if (tweetTextLC.indexOf("what is") > -1){
-        // tweet seeking info on a condition
-        //respondToCondition(client, tweet.user.screen_name);
-    }
-    else{
-        // don't understand, so just send a generic reply (currently same as help)
-        //replyWithHelp(client, tweet.user.screen_name);
-    }
-    */
-  }
 
-function streamTweet (stream) {
-    // callBack for when we get tweet data...
-    stream.on('data', onTweet);
+    return false;
+}
 
-    // ... when we get an error...
-    stream.on('error', function(error) {
-        log("function streamTweet error:" + error);
+// parse the tweet string for a postcode
+function getTweetPostcode(tweetString){
+    var postcodeRegEx = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z]0-9][A-Za-z])|([A-Z-z][A-Ha-hJ-Yj-y][09]?[A-Za-z])))) {0,1}[0-9][A-Za-z]{2})";
+    var match = tweetString.match(postcodeRegEx);
+
+    var tweetPostcode = null;
+    if(match != null){
+        tweetPostcode = match[0];// just assume the first postcode in the array is the one we need
+    }
+    log("getTweetPostcode: Match[0] is " + tweetPostcode);
+
+    return tweetPostcode;
+}
+
+// call to the API to convert postcode to lat/lon
+function getLongLatFromPostcode(postCode, callback)
+{
+    var longLatValue = {};
+    var postcodesAPI = new postcodesIO();
+    postcodesAPI.lookup(postCode, function (error, longLatValue) {
+        if(error){
+            log("getLongLatFromPostcode - postcodesAPI error = " + error);
+            return callback(error);
+        }
+
+        callback(null, longLatValue);
     });
 }
 
-//=========== end of functions ============================
-var secret = {
-    consumer_key: env.consumer_key, 
-    consumer_secret: env.consumer_secret, 
-    access_token_key: env.access_token, 
-    access_token_secret: env.access_token_secret   
-};
-var client = new Twitter(secret);
+// using long and lat, look up the nearest pharmacy
+function getNearestPharmFromLongLat(geoLocation, callback){
 
+    var serviceReply = {};
 
-log4js.loadAppender('file');
-
-log4js.configure({
-    appenders: [
-        {
-            type:'file',
-            filename: 'twitbot.log',
-            category: 'debug',
-            maxLogSize: 20480,
-            backups: 10
+    var requestUrl = env.nearby_service_api + "latitude=" + geoLocation.latitude + "&" + "longitude=" + geoLocation.longitude;
+    request(requestUrl, function (error, response, body) {
+        if(error){
+            log(error);
+            return callback(error);
         }
-    ]
-})
-var logger = log4js.getLogger('debug');
 
-log("Started running...");
+        if (response.statusCode != 200) {
+            return callback(new Error("getNearestPharmFromLogLat = url did not return 200"));
+        }
+        
+        // convert the nearest response into a reply
+       serviceReply.serviceLocResolved = true;
 
-var replyCount = 0;
+        var responseStuff = JSON.parse(body);
+        var firstNearbyPharm = responseStuff.nearby[0];
+        var nearbyName = firstNearbyPharm.name;
+        var nearbyMessage = firstNearbyPharm.openingTimesMessage;
+        var address = firstNearbyPharm.address;
+        // if fields are null, then don't add them to the location info
+        var add1 = (address.line1=="" ? "": (address.line1 + ", "));
+        var add2 = (address.line2=="" ? "": (address.line2 + ", "));
+        var add3 = (address.line3=="" ? "": (address.line3 + ", "));
+        var add4 = (address.city=="" ? "": (address.city + ", "));
+        var postcode = (address.postcode=="" ? "": address.postcode);
 
-// set things going
-var trackAndFollowObj= {
-    //track: 'help,nearest pharmacy,what is'
-    //follow: 'AndyDevTest'
-    //'follow=AndyDevTest'
-    //{track: 'help,nearest pharmacy,what is'},
-    //{follow: '799761379150495744'}
-    //track: 'help,nearest pharmacy,what is',
-    //follow: '799761379150495744'
-    follow: env.this_account_id
-}; 
+        serviceReply.text = "Your nearest Pharmacy is " + nearbyName + ". "
+            + add1 + add2 + add3 + add4 + postcode + ". " + nearbyMessage;
+        log("Data from nearest to return is " + serviceReply.text);
+    
+        callback(null, serviceReply);
+    });
+}
 
-//var result = getTweetPostcode("nearest pharmacy S42 6RP and something else")
-client.stream('statuses/filter', trackAndFollowObj, streamTweet);
+function createStatusUpdateObj(screen_name, currentTweetText, tweetInfo)
+{
+    var StatusObj = {};
+    
+    if(tweetInfo.isFollower == true){
+        if(tweetInfo.serviceLocResolved){
+            statusObj = {status: "D " + screen_name + " " + currentTweetText };
+        }else{
+            if(tweetInfo.badPostcode == true){
+                statusObj = {status: "D " + screen_name + " Thanks for the tweet about nearest Pharmacy. Sorry, could not find a postcode that matches the one you sent. Please check and try again"};
+            }else{
+                statusObj = {status: "D " + screen_name + " Thanks for the tweet about nearest Pharmacy. I need a bit more info on your location. Tweet me your postcode or turn on tweet location in your profile. Thanks "};
+            }
+        }
+    }
+    else{
+        statusObj = {status: "@" + screen_name + ". I'm a bot. For best results, follow me & tweet me in form 'Nearest Pharmacy <your postcode> and I'll DM a reply"};
+    }
+    statusObj.status;
 
+    return statusObj;
+}
